2fc35eaa117652fdc856903ad0adacff60567310
Andrew Nadraliev <andrew.nadraliev@hotmail.com>
2016-09-28 20:57:29 +0300

    added banknote class

---
+++ ATMSimulation/ATMSimulation/Banknote.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMSimulation
+{
+    public class Banknote
+    {
+        public int Rubles { get { return rubles; } }
+
+
+
+        public enum FaceValue { Fifty=50, Hundred=100, FiveHundred=500, Thousand=1000, FiveThousand=5000 };
+
+        private int rubles;
+
+        public Banknote(FaceValue faceValue)
+        {
+            rubles = (int)faceValue;
+        }
+    }
+}
