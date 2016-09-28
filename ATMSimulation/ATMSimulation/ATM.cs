67376ad4b49dddc3f9d63fbe1b43c7d13e577563
Andrew Nadraliev <andrew.nadraliev@hotmail.com>
2016-09-28 21:05:13 +0300

    added ATM class

---
+++ ATMSimulation/ATMSimulation/ATM.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMSimulation
+{
+    public class ATM
+    {
+
+        public List<Banknote.FaceValue> AvailableBanknotes;
+
+        private List<Banknote> banknotes;
+
+        public ATM()
+        {
+
+        }
+
+        public void StartSession()
+        {
+
+        }
+
+        public void CheckPin()
+        {
+
+        }
+    }
+}
