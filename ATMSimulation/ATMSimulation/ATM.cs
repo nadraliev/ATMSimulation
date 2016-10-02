fd062e1738dcc5f7a82b616301ea34778ed501d1
soutvoidVM <soutvoidVM@DESKTOP-EI77LV0>
2016-10-02 12:05:33 +0300

    added methods LoadBanknotes, EndSession, RemoveBanknote, UpdateAvailableBanknotes to ATM class

--- ATMSimulation/ATMSimulation/ATM.cs
+++ ATMSimulation/ATMSimulation/ATM.cs
@@ -11,12 +11,7 @@ namespace ATMSimulation
 
         public List<Banknote.FaceValue> AvailableBanknotes { get
             {
-                foreach (Banknote banknote in banknotes)
-                {
-                    if (!availableBanknotes.Contains(banknote.Rubles))
-                        availableBanknotes.Add(banknote.Rubles);
-                }
-                availableBanknotes.Sort();  //ascending
+                availableBanknotes = UpdateAvailableBanknotes(banknotes);
                 return availableBanknotes;
             } }
         
@@ -41,16 +36,25 @@ namespace ATMSimulation
         {
             availableBanknotes = new List<Banknote.FaceValue>();
             banknotes = new List<Banknote>();
+            Console.WriteLine("new ATM created");
+        }
+
+        public void LoadBanknotes(List<Banknote> moneyToLoad)
+        {
+            banknotes.AddRange(moneyToLoad);
+            Console.WriteLine("banknotes loaded");
         }
 
         public void StartSession(Customer customer)
         {
             currentCustomer = customer;
+            Console.WriteLine("new customer entered");
         }
 
         public void EndSession()
         {
             currentCustomer = null;
+            Console.WriteLine("session ended");
         }
 
         private bool CheckPin(int enteredPin, DebitCard targetCard)
@@ -58,17 +62,60 @@ namespace ATMSimulation
             return enteredPin == targetCard.Pin;
         }
 
-        private int RequestMoney(int rubles)    //returns code of error
+        public int RequestMoney(int rubles, ref List<Banknote> result)    //returns code of error
         {
             if (rubles > Rubles) return 1;  //not enough money
             if (rubles % (int)AvailableBanknotes.First() != 0) return 2;     //atm doesn't have requested banknotes
-
+            List<Banknote> banknotesClone = new List<Banknote>();
+            banknotesClone.AddRange(banknotes);
+            List<Banknote.FaceValue> availableBanknotesClone = UpdateAvailableBanknotes(banknotesClone);
+            int i = availableBanknotesClone.Count - 1;
+            Banknote.FaceValue currentFace;
+            while (rubles > 0)
+            {
+                currentFace = availableBanknotesClone[i];
+                while (rubles >= (int)currentFace && availableBanknotesClone.Contains(currentFace))
+                {
+                    rubles -= (int)currentFace;
+                    result.Add(new Banknote(currentFace));
+                    RemoveBanknote(banknotesClone ,currentFace);
+                    availableBanknotesClone = UpdateAvailableBanknotes(banknotesClone);
+                }
+                if (rubles % (int)availableBanknotesClone.First() != 0) return 2;
+                i--;
+            }
             return 0;   //no error
+        }
+
+        private void RemoveBanknote(List<Banknote> banknotes, Banknote.FaceValue face)
+        {
+            for (int i = 0; i < banknotes.Count; i++)
+            {
+                if (banknotes[i].Rubles.Equals(face))
+                {
+                    banknotes.RemoveAt(i);
+                    return;
+                }
+            }
+        }
+
+        private List<Banknote.FaceValue> UpdateAvailableBanknotes(List<Banknote> banknotes)
+        {
+            List<Banknote.FaceValue> availableBanknotes = new List<Banknote.FaceValue>();
+            foreach (Banknote banknote in banknotes)
+            {
+                if (!availableBanknotes.Contains(banknote.Rubles))
+                    availableBanknotes.Add(banknote.Rubles);
+            }
+            availableBanknotes.Sort();  //ascending
+            return availableBanknotes;
         }
 
         private void GiveMoney(List<Banknote> money)
         {
-
+            foreach (Banknote b in money)
+                RemoveBanknote(banknotes, b.Rubles);
+            availableBanknotes = UpdateAvailableBanknotes(banknotes);
         }
     }
 }
--- ATMSimulation/ATMSimulation/MainWindow.xaml.cs
+++ ATMSimulation/ATMSimulation/MainWindow.xaml.cs
@@ -24,5 +24,7 @@ namespace ATMSimulation
         {
             InitializeComponent();
         }
+
+        
     }
 }
